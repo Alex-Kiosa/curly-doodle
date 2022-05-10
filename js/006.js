f8796d04a11c919549a65effb6592d778bb81f07
Alex-Kiosa <akiosa88@gmail.com>
2022-05-10 21:37:50 +0400

    wrote function updateRecords(), which udate 0r set propeties for collection objects

--- js/006.js
+++ js/006.js
@@ -1,23 +1,49 @@
-function accesingNestedObj() {
-  // Only change code below this line
+// Setup
+const recordCollection = {
+	2548: {
+		albumTitle: 'Slippery When Wet',
+		artist: 'Bon Jovi',
+		tracks: ['Let It Rock', 'You Give Love a Bad Name']
+	},
+	2468: {
+		albumTitle: '1999',
+		artist: 'Prince',
+		tracks: ['1999', 'Little Red Corvette']
+	},
+	1245: {
+		artist: 'Robert Palmer',
+		tracks: []
+	},
+	5439: {
+		albumTitle: 'ABBA Gold'
+	}
+};
 
-  const myStorage = {
-    "car": {
-      "inside": {
-        "glove box": "maps",
-        "passenger seat": "crumbs"
-       },
-      "outside": {
-        "trunk": "jack"
-      }
-    }
-  };
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+	if(prop != 'tracks' && value) {
+		records[id][prop] = value;
+	}
+	
+	if(prop == 'tracks' && records[id]['tracks'] == undefined) {
+		records[id][prop] = [];
+	}
+	
+	if(prop == 'tracks' && value) {
+		records[id][prop].push(value);
+	}
 
-  const gloveBoxContents = myStorage.car.inside["glove box"];
+	if(!value) {
+		delete records[id][prop];
+	}
 
-  return gloveBoxContents;
-
-  // Only change code above this line
+	return records;
 }
 
-console.log(accesingNestedObj());
+console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
+console.log(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"));
+console.log(updateRecords(recordCollection, 2548, "tracks", ""));
+
+const myArray = [];
+
+// Only change code below this line
