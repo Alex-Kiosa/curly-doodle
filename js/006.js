3c76154b142fa58e8fe470b6326006428b86cf39
Alex-Kiosa <akiosa88@gmail.com>
2022-05-10 13:16:10 +0400

    edited index.html, added 06.js and wrote function accesingNestedObj()

--- js/006.js
+++ js/006.js
@@ -1,6 +1,23 @@
-// Setup
-let lastNameLength = 0;
-const lastName = "Lovelace";
+function accesingNestedObj() {
+  // Only change code below this line
 
-// Only change code below this line
-lastNameLength = lastName.length;
+  const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+
+  const gloveBoxContents = myStorage.car.inside["glove box"];
+
+  return gloveBoxContents;
+
+  // Only change code above this line
+}
+
+console.log(accesingNestedObj());
